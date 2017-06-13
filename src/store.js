5188604e791a8ab5323896d7e829bb4c40bff239
Kevin McCarthy <kjmagic13@gmail.com>
2017-06-12 20:26:24 -0400

    init vuex npm install --save vuex

---
+++ src/store.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export const store = new Vuex.Store({
+  modules: {},
+  state: {
+    lorem: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+  },
+  getters: {},
+  mutatons: {},
+  actions: {},
+})
